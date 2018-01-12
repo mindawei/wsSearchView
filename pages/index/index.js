59cfb22b4be33aa6ed6184316ea48a04ddb48809
wlwb <mindawei1014@163.com>
2018-01-12 22:00:27 +0800

    ‘初步提交’

---
+++ pages/index/index.js
@@ -0,0 +1,32 @@
+//index.js
+var WxSearch = require('../../wxSearchView/wxSearchView.js');
+
+Page({
+  data: {},
+
+  // 搜索栏
+  onLoad: function () {
+    var that = this;
+    WxSearch.init(
+      that,  // 本页面一个引用
+      ['杭州', '嘉兴', "海宁", "桐乡", '宁波', '金华'], // 热点搜索推荐，[]表示不使用
+      ['湖北', '湖南', '北京',"南京"],// 搜索匹配，[]表示不使用
+      that.mySearchFunction // 提供一个搜索回调函数
+    );
+  },
+
+  // 转发函数,固定部分
+  wxSearchInput: WxSearch.wxSearchInput,  // 输入变化时的操作
+  wxSerchFocus: WxSearch.wxSearchFocus,  // 输入框获得焦点 
+  wxSearchKeyTap: WxSearch.wxSearchKeyTap,  // 点击提示或者关键字、历史记录时的操作
+  wxSearchDeleteAll: WxSearch.wxSearchDeleteAll, // 删除所有的历史记录
+  wxSearchConfirm: WxSearch.wxSearchConfirm,  // 搜索函数
+  wxSearchClear: WxSearch.wxSearchClear,  // 清空函数
+  wxSearchBlur: WxSearch.wxSearchBlur, // 失去焦点函数
+  
+  // 搜索回调函数  
+  mySearchFunction: function (value) {
+    // do your job here
+    console.log("搜索值为:" + value)
+  }
+})
